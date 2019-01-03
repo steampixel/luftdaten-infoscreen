5e0f512feb389d0c807743096711c49fa6c38383
steampixel <info@steampixel.de>
2019-01-03 16:35:44 +0100

    first commit

---
+++ var/www/html/luftdaten.info/config.js
@@ -0,0 +1,32 @@
+var urls = [
+	{
+		url: 'info.html',
+		duration: 20000
+	},
+	{
+		url: 'http://deutschland.maps.luftdaten.info/#13/49.7956/9.9636',
+		title: 'Aktuelle Belastung mit Feinstaub in <strong>Würzburg</strong>',
+	},
+	{
+		url: 'http://deutschland.maps.luftdaten.info/#11/49.8014/10.0337',
+		title: 'Aktuelle Belastung mit Feinstaub im <strong>Landkreis Würzburg</strong>'
+	},
+	{
+		url: 'https://deutschland.maps.luftdaten.info/#9/49.5760/10.9591',
+		title: 'Aktuelle Belastung mit Feinstaub in <strong>Franken</strong>'
+	},
+	{
+		url: 'https://deutschland.maps.luftdaten.info/#8/48.865/11.841',
+		title: 'Aktuelle Belastung mit Feinstaub in <strong>Bayern</strong>'
+	},
+	{
+		url: 'https://deutschland.maps.luftdaten.info/#5/50.732/10.745',
+		title: 'Aktuelle Belastung mit Feinstaub in <strong>Deutschland</strong>'
+	}
+];
+
+var defaultFrameDurationTime = 30000;
+
+var progressBarTicks = 100;
+
+var nextUrl = 0;
