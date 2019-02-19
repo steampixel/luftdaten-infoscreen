86486eebb606b497144bd47985ace0580084d135
Chris <info@steampixel.de>
2019-02-19 20:55:15 +0100

    Added some comments

--- var/www/html/luftdaten.info/config.js
+++ var/www/html/luftdaten.info/config.js
@@ -1,11 +1,11 @@
 var urls = [
 	{
-		url: 'info.html',
-		duration: 20000
+		url: 'info.html', // Set the url
+		duration: 20000 // This duration will override the default duration
 	},
 	{
 		url: 'https://deutschland.maps.luftdaten.info/?nooverlay#13/49.7956/9.9636',
-		title: 'Aktuelle Belastung mit Feinstaub in <strong>Würzburg</strong>',
+		title: 'Aktuelle Belastung mit Feinstaub in <strong>Würzburg</strong>', // Optionally set a title for this page
 	},
 	{
 		url: 'https://deutschland.maps.luftdaten.info/?nooverlay#11/49.8014/10.0337',
@@ -25,8 +25,11 @@ var urls = [
 	}
 ];
 
+// This is the default duration
 var defaultFrameDurationTime = 30000;
 
+// Nuber of progress bar ticks
 var progressBarTicks = 100;
 
+// Start with this url
 var nextUrl = 0;
