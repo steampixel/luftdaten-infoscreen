47e2617618bef5aab71822a69c82ac82c47c4758
Chris <info@steampixel.de>
2019-02-19 20:27:02 +0100

    Disabled donation button

--- var/www/html/luftdaten.info/config.js
+++ var/www/html/luftdaten.info/config.js
@@ -4,23 +4,23 @@ var urls = [
 		duration: 20000
 	},
 	{
-		url: 'http://deutschland.maps.luftdaten.info/#13/49.7956/9.9636',
+		url: 'https://deutschland.maps.luftdaten.info/?nooverlay#13/49.7956/9.9636',
 		title: 'Aktuelle Belastung mit Feinstaub in <strong>Würzburg</strong>',
 	},
 	{
-		url: 'http://deutschland.maps.luftdaten.info/#11/49.8014/10.0337',
+		url: 'https://deutschland.maps.luftdaten.info/?nooverlay#11/49.8014/10.0337',
 		title: 'Aktuelle Belastung mit Feinstaub im <strong>Landkreis Würzburg</strong>'
 	},
 	{
-		url: 'https://deutschland.maps.luftdaten.info/#9/49.5760/10.9591',
+		url: 'https://deutschland.maps.luftdaten.info/?nooverlay#9/49.5760/10.9591',
 		title: 'Aktuelle Belastung mit Feinstaub in <strong>Franken</strong>'
 	},
 	{
-		url: 'https://deutschland.maps.luftdaten.info/#8/48.865/11.841',
+		url: 'https://deutschland.maps.luftdaten.info/?nooverlay#8/48.865/11.841',
 		title: 'Aktuelle Belastung mit Feinstaub in <strong>Bayern</strong>'
 	},
 	{
-		url: 'https://deutschland.maps.luftdaten.info/#5/50.732/10.745',
+		url: 'https://deutschland.maps.luftdaten.info/?nooverlay#5/50.732/10.745',
 		title: 'Aktuelle Belastung mit Feinstaub in <strong>Deutschland</strong>'
 	}
 ];
